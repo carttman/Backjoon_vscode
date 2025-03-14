a9ae7c0f0ab7d637ce27dee985416eb07a40abfe
carttman <chj990920@gmail.com>
2025-03-14 18:10:54 +0900

    Update 1181.cpp

--- 1181.cpp
+++ 1181.cpp
@@ -16,40 +16,64 @@ int main()
     cin >> N;
 
     vector<string> A(N);
+    set<string> s;
+    // 짧은 것부터
+    // 길이가 같으면 사전 순
 
-    for(int i=0; i < N; i++)
+    for(int i=0; i<N; i++)
     {
         cin >> A[i];
+        s.insert(A[i]);
+       
     }
 
-    sort(A.begin(), A.end(), [](string a, string b){
-        if(a.size() < b.size())
-        {
-            return a < b;
-        }
+    for(auto it = s.begin(); it != s.end(); it++)
+    {
+        cout << *it << "\n";
+    }
 
-        if(a.size() == b.size())
-        {
-            return a < b;
-        }
+    // sort(A.begin(), A.end(), [](string a, string b){
 
-        return a < b;
-    });
+    //     if(a.size() < b.size())
+    //     {
+    //         return b;
+    //     }
 
-    set<string> s;
+    // });
+
+    // for(int i=0; i < N; i++)
+    // {
+    //     cin >> A[i];
+    // }
+
+    // sort(A.begin(), A.end(), [](string a, string b){
+    //     if(a.size() < b.size())
+    //     {
+    //         return a < b;
+    //     }
+
+    //     if(a.size() == b.size())
+    //     {
+    //         return a < b;
+    //     }
+
+    //     return a < b;
+    // });
+
+    // set<string> s;
 
     
 
-    for(int i=0; i<A.size(); i++)
-    {
-        s.insert(A[i]);
-        //cout << A[i] << "\n";
-    }
+    // for(int i=0; i<A.size(); i++)
+    // {
+    //     s.insert(A[i]);
+    //     //cout << A[i] << "\n";
+    // }
 
-    for(auto i = s.begin(); i != s.end(); i++)
-    {
-        cout << &i << "\n";
-    }
+    // for(auto i = s.begin(); i != s.end(); i++)
+    // {
+    //     cout << &i << "\n";
+    // }
 
     return 0;
 }
